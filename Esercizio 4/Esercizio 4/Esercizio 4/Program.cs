16a9b9e3d745104d8821baacd317ad675217326d
“Hassen <hassendj12@gmail.com>
2018-02-25 18:38:42 +0100

    ESERCIZIO TRIS COMINCIATO

---
+++ Esercizio 4/Esercizio 4/Esercizio 4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] campo = new char[3, 3];
+            inizializzazione(ref campo);
+            do
+            {
+                turno(ref campo);
+
+            }
+            while (controlloVincita(campo));
+
+            Console.ReadKey();
+        }
+
+
+        static void inizializzazione(ref char[,] c)
+        {
+            for (int i = 0; i < c.Length; i++)
+            {
+                for (int j = 0; j < c.Length; j++)
+                {
+                    c[i, j] = ' ';
+                }
+            }
+        }
+
+        static void turno(ref char[,] c)
+        {
+
+        }
+
+        static bool controlloVincita(char[,] c)
+        {
+
+            return true;
+        }
+
+
+    }
+}
